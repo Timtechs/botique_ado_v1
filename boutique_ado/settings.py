cd117e9d2617e23dd3b1a99858e0768f5d2a5e86
Timtechs <Timmy.s.deckert@gmail.com>
2022-11-25 10:07:31 +0000

    Added allauth templates and base template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5fe0-j045c&(g%tw3^y07mi6^kto&^=7_0e4fnuw#qv0&zak#v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
