3bc7068e6d85b556a1c42204883ea2e35b14fdf6
Timtechs <Timmy.s.deckert@gmail.com>
2022-11-30 13:09:05 +0000

    Added toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -9,10 +12,38 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    product = Product.objects.get(pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    else:
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+
+
 def adjust_bag(request, item_id):
-    """
-    Adjust the quantity of the specified product to the specified amount
-    """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -37,36 +68,8 @@ def adjust_bag(request, item_id):
     return redirect(reverse('view_bag'))
 
 
-def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    bag = request.session.get('bag', {})
-
-    if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
-        else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-    else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-        else:
-            bag[item_id] = quantity
-
-    request.session['bag'] = bag
-    return redirect(redirect_url)
-
-
 def remove_from_bag(request, item_id):
-    """ remove the item from the shopping bag """
+    """Remove the item from the shopping bag"""
 
     try:
         size = None
